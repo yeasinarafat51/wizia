c513b3ae73825ab507f71e84201a7f6c59b554d5
yeasinarafat51 <aloondokar65@gmail.com>
2024-11-27 09:01:58 +0600

    train

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Carousel from "./components/Carousel"
+import Control from "./components/Control"
 import HeroSection from "./components/HeroSection"
 import Partners from "./components/Partners"
+import Train from "./components/Train"
 import NavbarArea from "./shared/Navbar"
 
 
@@ -12,10 +14,12 @@ function App() {
     <div>
     <NavbarArea/>
     <HeroSection/>
-   <div className="xl:mt-36 mt-16">
+   <div className="xl:mt-36 mt-16 border-b-2 border-b-[#002228] shadow-lg">
     <Partners/>
    {/* <Carousel/> */}
    </div>
+   <Control/>
+   <Train/>
     </div>
    </>
     
