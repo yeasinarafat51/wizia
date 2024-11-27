1be78557e2cc5c4d2a5d075084b1a1f561c85802
yeasinarafat51 <aloondokar65@gmail.com>
2024-11-27 15:21:38 +0600

    polis2

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,33 @@
-// import Button from "./common/Button"
-import Buttonx from "./common/Button"
-import Allocate from "./components/Allocate"
-import Card from "./components/Card"
-import Carousel from "./components/Carousel"
-import Control from "./components/Control"
-import Embarce from "./components/Embarce"
-import HeroSection from "./components/HeroSection"
-import Partners from "./components/Partners"
-import Train from "./components/Train"
-import Footer from "./shared/Footer"
-import NavbarArea from "./shared/Navbar"
-
+import Allocate from "./components/Allocate";
+import Carousel from "./components/Carousel";
+import Control from "./components/Control";
+import Embarce from "./components/Embarce";
+import HeroSection from "./components/HeroSection";
+import Partners from "./components/Partners";
+import Train from "./components/Train";
+import Footer from "./shared/Footer";
+import NavbarArea from "./shared/Navbar";
 
 function App() {
-
-
   return (
     <>
-    <div>
-    <NavbarArea/>
-    <HeroSection/>
-   <div className="xl:mt-36 mt-16 border-b-2 border-b-[#002228] shadow-lg">
-    <Partners/>
-   {/* <Carousel/> */}
-   </div>
-   <Control/>
-   <Train/>
-   <div className="my-8">
-   <Carousel/>
-   </div>
-   <Allocate/>
-   <Embarce/>
-   {/* <Card/> */}
-   {/* <Buttonx/> */}
-   <Footer/>
-    </div>
-   </>
-    
-  )
+      <div>
+        <NavbarArea />
+        <HeroSection />
+        <div className="xl:mt-36 mt-16 border-b-2 border-b-[#002228] shadow-lg">
+          <Partners />
+        </div>
+        <Control />
+        <Train />
+        <div className="my-8">
+          <Carousel />
+        </div>
+        <Allocate />
+        <Embarce />
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
